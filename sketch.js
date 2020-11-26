b99faa1083ea5ee70c1808c1ba5d1a6f5cf5ccac
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-11-26 19:26:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const constraint=Matter.constraint;
 
 var engine, world;
 var box1, pig1;
@@ -29,6 +30,7 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+log6=new Log(230,180,80,PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
