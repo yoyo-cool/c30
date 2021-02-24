957f707d1909dc3256df729c2cf661e4319b69f6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-02-24 11:12:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,19 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-const constraint=Matter.constraint;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+var backgroundImg ;
+function preload(){    backgroundImg = loadImage("sprites/bg.png");
 }
-
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
+    ground = new Ground(600,height,1200,20)
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -25,12 +21,14 @@ function setup(){
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+   
+    glass1 = new Glass(700, 240, 50, 50);
+    glass2 = new Glass(920, 240, 50, 50);
+    glass3 = new Glass(700, 240, 50, 50);
+    glass4 = new Glass(920, 240, 50, 50);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-log6=new Log(230,180,80,PI/2);
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
@@ -41,6 +39,8 @@ log6=new Log(230,180,80,PI/2);
 }
 
 function draw(){
+  
+
     background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
@@ -51,9 +51,13 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    platform.display();
 
-    box3.display();
-    box4.display();
+    glass1.display();
+  glass2.display();
+  glass3.display();
+  glass4.display();
+   
     pig3.display();
     log3.display();
 
@@ -62,5 +66,4 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
 }
